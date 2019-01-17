4d4062dd664ce874732e9460ec1dc12c1b129bf7
fabiochiquezi <fabiochiquezi@gmail.com>
2019-01-17 17:41:26 -0200

    WP - Precisa reformar

---
+++ wp-projeto/wp-content/plugins/woocommerce/templates/single-product.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * The Template for displaying all single products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header( 'shop' ); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+<style>
+	.banner-produto{
+		background: <?php the_field('color_back'); ?> !important;
+	}
+	.banner-produto h1, .banner-produto .desc{
+		color: <?php the_field('color_font'); ?> !important;
+	}
+</style>
+
+<div id="" class="">
+	<main id="" class="" role="">
+		<div id="product-<?php the_ID(); ?>" <?php wc_product_class(); ?>>
+		
+			<section class="banner-produto">
+				<div class="container">
+					<div class="row d-flex flex-column align-items-center flex-lg-row justify-content-lg-between">
+						<div class="content order-2 order-lg-1">
+						
+							<h1><?php the_field('titulo_01') ?> <span><?php the_field('subtitulo_01'); ?></span></h1>
+							<p class="desc"><?php the_field('desc_01'); ?></p>
+							<p class="preco"><?php the_field('preco'); ?></p>
+
+							<?php wc_get_template_part( 'content', 'single-product' ); ?>
+
+						</div>
+						<div class="image order-1">
+							<img src="<?php the_field('banner_desk'); ?>" class="d-none d-lg-block" alt="">
+							<img src="<?php the_field('banner_mobile'); ?>" class="d-lg-none" alt="">
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="detalhes-produto">
+				<div class="container">
+					<div class="row d-lg-flex flex-lg-nowrap justify-content-lg-between">
+						<div class="content d-flex flex-column align-items-center align-items-lg-start align-content-center">
+							<div class="menu-colapse">
+									<ul class="d-none d-lg-flex">
+											<li><a href="item-1" class="active">Nota do Autor</a></li>
+											<li>|</li>
+											<li><a href="item-2">Detalhes</a></li>
+											<li>|</li>
+											<li><a href="item-3">Resumo</a></li>
+										</ul>
+									<select class="d-lg-none">
+										<option value="item-1">Nota do Autor</option>
+										<option value="item-2">Detalhes</option>
+										<option value="item-3">Resumo</option>
+									</select>
+							</div>
+
+							<div class="content-colapse">
+								<div class="item item-1 active">
+									<h2><?php the_field('nota_autor_titulo'); ?></h2>
+									<?php the_field('nota_autor_desc'); ?>
+
+									<div class="frase-personalizada">
+										<p><?php the_field('nota_autor_frase'); ?></p>
+										<p class="autor"><?php the_field('autor_nome'); ?></p>
+									</div>
+								</div>
+
+								<div class="item item-2">
+										<h2>R$ 29,90 <span>ou</span> <?php the_field('preco_parcelado'); ?></h2>
+										<?php the_field('sub_inform'); ?>
+									</div>
+
+									<div class="item item-3">
+											<h2><?php the_field('titulo_resumo_2'); ?></h2>
+											<?php the_field('resumo_livro'); ?>
+										</div>
+							</div>
+						</div>
+						<div class="image">
+							<img src="<?php the_field('imagem_ilustrativa_01'); ?>" alt="">
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<hr class="hr-1">
+
+			<section class="resumo-produto d-none d-lg-block">
+				<div class="container">
+					<div class="row">
+							<h3>RESUMO DO LIVRO</h3>
+							<h2><?php the_field('titulo_resumo_2'); ?></h2>
+							<div class="content">
+								<?php the_field('resumo_livro'); ?>
+							</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="outros-produtos">
+				<div class="container">
+					<div class="row d-flex flex-column align-items-center flex-lg-row justify-content-around">
+
+						<?php
+						$post = get_the_ID();
+
+							$args = array(
+								'post_type' => 'product',
+								'order' => 'ASC',
+								'posts_per_page' => '1',
+								'orderby'        => 'rand',
+								'post__not_in' => array($post)
+							);
+							$the_query = new WP_Query($args)
+						?>
+						<?php if( $the_query->have_posts() ) : while($the_query->have_posts()) : $the_query->the_post(); ?>
+
+							<div class="content order-2 order-lg-1">
+								<h3>OUTROS LIVROS</h3>
+								<h2><?php the_title(); ?></h2>
+								<p><?php the_field('descricao_outros_produtos') ?></p>
+								<a href="<?php the_permalink(); ?>" class="button1">VER MAIS</a>
+							</div>
+
+							<div class="image order-1">
+								<img src="<?php the_field('imagem_marketing'); ?>" alt="">
+							</div>
+
+						<?php endwhile; else: endif; 
+        			wp_reset_query();
+        		?>	
+					</div>
+				</div>
+			</section>
+
+			<section class="testimonials-section">
+				<div class="container">
+					<div class="row">
+							<h3>Testemunhos</h3>
+							<h2><?php the_field('titulo_testemunhos'); ?></h2>
+
+							<div class="content-testimonials d-md-flex flex-wrap justify-content-lg-between">
+
+								<?php the_field('testemunhos'); ?>
+							</div>
+					</div>
+				</div>
+
+				<hr class="hr-1 d-none d-lg-block">
+
+				<footer class="contato-section">
+					<div class="container">
+						<div class="row">
+								<h2><?php the_field('title_contato'); ?></h2>
+
+								<div class="sub">
+										<p><?php the_field('desc_contato'); ?></p>
+								</div>
+					
+								<div class="contact-1 d-lg-flex align-items-center">
+									<div class="tel">
+										<p><?php the_field('tel01'); ?></p>
+										<p><?php the_field('tel02'); ?></p>
+									</div>
+									<ul class="social d-flex">
+										<li><a href="<?php the_field('face_link'); ?>" target="_blank"><i class="icon icon-face"></i></a></li>
+										<li><a href="<?php the_field('insta_link'); ?>" target="_blank"><i class="icon icon-insta"></i></a></li>
+										<li><a href="<?php the_field('whats_link'); ?>" target="_blank"><i class="icon icon-whats"></i></a></li>
+									</div>
+								</div>
+					
+								<p class="ajuste-separa">Ou</p>
+					
+								<form action="">
+									<input type="text" name="nome" placeholder="Nome Completo">
+									<input type="text" name="email" placeholder="E-mail">
+									<textarea name="mensagem" placeholder="Sua Mensagem"></textarea>
+									<input type="text" value="enviar" class="button-submit">
+									<a href="" class="button1">ENVIAR</a>
+								</form>
+					
+								<div class="direitos-autorais">
+									<p>©2019 Todos os direitos autorais reservados.</p>
+								</div>
+						</div>
+					</div>
+				</footer>
+			</section>
+		</div>
+	</main>
+</div>
+<?php endwhile; // end of the loop. ?>
+
+
+<div class="buttons-adc d-flex">
+  <a href="http://localhost/divi-yoga-tema-design/wp-projeto/carrinho/" class="button button-carrinho">
+		<i class="icon icon-carrinho"></i>
+    <span class="quantidade-carrinho">
+			<?php 
+				global $woocommerce;
+				$numero = sprintf(_n('%d', $woocommerce->cart->cart_contents_count, 'woothemes'));
+
+				echo $numero;
+			?>
+		</span>
+  </a>
+  <a href="<?php the_field('whats_link'); ?>" target="_blank" class="button button-whats"><i class="icon icon-whats2"></i></a>
+</div>
+
+
+
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			
+
+		<?php endwhile; // end of the loop. ?>
+
+
+
+
+<?php get_footer( 'shop' );
+
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
---
+++ wp-projeto/wp-content/themes/simple-hotsite-e-commerce/index.php
@@ -0,0 +1,242 @@
+<?php get_header(); ?>
+
+  <section class="banner-produto">
+    <div class="container">
+      <div class="row d-flex flex-column align-items-center flex-lg-row justify-content-lg-between">
+        <div class="content order-2 order-lg-1">
+          <h1>Hezbbollah <span>Uma breve
+              História</span></h1>
+          <p class="desc">Nunc consequat justo eget enim finibus porta. 
+              Suspendisse orci nunc, rutrum quis nunc sed.</p>
+          <p class="preco">R$ 29,90</p>
+          <a href="" class="button1">COMPRAR</a>
+        </div>
+        <div class="image order-1">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/banner-01.png" class="d-none d-lg-block" alt="">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/banner-01-mob.png" class="d-lg-none" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="detalhes-produto">
+    <div class="container">
+      <div class="row d-lg-flex flex-lg-nowrap justify-content-lg-between">
+        <div class="content d-flex flex-column align-items-center align-items-lg-start align-content-center">
+          <div class="menu-colapse">
+              <ul class="d-none d-lg-flex">
+                  <li><a href="item-1" class="active">Nota do Autor</a></li>
+                  <li>|</li>
+                  <li><a href="item-2">Detalhes</a></li>
+                  <li>|</li>
+                  <li><a href="item-3">Outros</a></li>
+                </ul>
+              <select class="d-lg-none">
+                <option value="item-1">Nota do Autor</option>
+                <option value="item-2">Detalhes</option>
+                <option value="item-3">Outros</option>
+              </select>
+          </div>
+
+          <div class="content-colapse">
+            <div class="item item-1 active">
+              <h2>Perspeciates Unde & Omnis
+                  Zoga Natus</h2>
+              <p>Perspiciatis unde omnis iste natus sit voluptatem accusantium 
+                  doloremque laudantium, totam rem aperiam, eaque ipsa quae. Ab illo 
+                  inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut.
+                  </p>
+
+              <div class="frase-personalizada">
+                <p>“Nunc consequat justo eget enim finibus porta. Suspendisse 
+                    orci nunc, rutrum quis nunc sed, feugiat hendrerit massa. In 
+                    cursus ornare sollicitudin.”</p>
+                <p class="autor">Ivan Hassan, Nunc Consequat</p>
+              </div>
+            </div>
+
+            <div class="item item-2">
+                <h2>R$ 29,90 <span>ou</span> 3 x R$10,00</h2>
+                <ul>
+                  <li><b>- Altura:</b> 23.00 cm</li>
+                  <li><b>- Cód. Barras:</b> 9788556510761</li>
+                  <li><b>- Largura:</b> 16.00 cm</li>
+                  <li><b>- Peso:</b> 1.000 Kg</li>
+                  <li><b>- Produto sob encomenda:</b> Sim</li>
+                </ul>
+  
+                <p>“Nunc consequat justo eget enim finibus porta. Suspendisse 
+                    orci nunc, rutrum quis nunc sed, feugiat hendrerit massa. In 
+                    cursus ornare sollicitudin.”</p>
+              </div>
+
+              <div class="item item-3">
+                  <h2>Donec A Tristique Risus!</h2>
+                  <p>Donec tristique nec risus at tempor. Praesent vel blandit lacus, id viverra nunc. Pellentesque tristique nibh ac erat lacinia maximus. Quisque quis pellentesque metus. Nam vestibulum libero at nulla sollicitudin sodales. Vivamus auctor pellentesque varius. Fusce in egestas ante. Etiam mattis dui et purus finibus dignissim quis et erat. Aliquam quis rhoncus lacus. Proin at massa in quam viverra egestas. Nulla facilisi. Integer at libero quis ipsum mollis aliquam suscipit eu nibh. Vivamus id augue justo. Maecenas aliquet pulvinar nisl venenatis faucibus. Vivamus consectetur sapien lectus, a gravida magna euismod sit amet.
+                  </p>
+                  <p>Fusce sed leo ac enim eleifend tempus. Vestibulum condimentum, nulla nec efficitur suscipit, mi arcu bibendum diam, eget posuere nisl massa ut neque. Nulla facilisi. Etiam sed fermentum libero. Vestibulum eget volutpat massa. Sed eu ipsum bibendum, finibus elit at, semper urna. Aliquam dapibus, velit facilisis venenatis interdum, augue sapien consectetur metus, in accumsan dolor nisl sed felis. Duis ultricies risus nec pulvinar dignissim.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec mauris nec nulla congue consectetur nec et odio. Phasellus lobortis commodo mi, in pharetra eros. Nunc eu dictum tellus, vitae finibus dui. Nam aliquet scelerisque quam ac molestie. Suspendisse pellentesque finibus tellus, a malesuada dolor mollis et. Donec eu lectus ac libero iaculis tempus eu vitae magna. </p>
+    
+                  <div class="frase-personalizada">
+                    <p>“Nunc consequat justo eget enim finibus porta. Suspendisse 
+                        orci nunc, rutrum quis nunc sed, feugiat hendrerit massa. In 
+                        cursus ornare sollicitudin.”</p>
+                    <p class="autor">Ivan Hassan, Nunc Consequat</p>
+                  </div>
+                </div>
+          </div>
+        </div>
+        <div class="image">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/foto-ilustrativa-01.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <hr class="hr-1">
+
+  <section class="resumo-produto d-none d-lg-block">
+    <div class="container">
+      <div class="row">
+          <h3>RESUMO DO LIVRO</h3>
+          <h2>Donec A Tristique Risus!</h2>
+          <div class="content">
+            <p>Donec tristique nec risus at tempor. Praesent vel blandit lacus, id viverra nunc. Pellentesque tristique nibh ac erat lacinia maximus. Quisque quis pellentesque metus. Nam vestibulum libero at nulla sollicitudin sodales. Vivamus auctor pellentesque varius. Fusce in egestas ante. Etiam mattis dui et purus finibus dignissim quis et erat. Aliquam quis rhoncus lacus. Proin at massa in quam viverra egestas. Nulla facilisi. Integer at libero quis ipsum mollis aliquam suscipit eu nibh. Vivamus id augue justo. Maecenas aliquet pulvinar nisl venenatis faucibus. Vivamus consectetur sapien lectus, a gravida magna euismod sit amet.</p>
+            <p>Fusce sed leo ac enim eleifend tempus. Vestibulum condimentum, nulla nec efficitur suscipit, mi arcu bibendum diam, eget posuere nisl massa ut neque. Nulla facilisi. Etiam sed fermentum libero. Vestibulum eget volutpat massa. Sed eu ipsum bibendum, finibus elit at, semper urna. Aliquam dapibus, velit facilisis venenatis interdum, augue sapien consectetur metus, in accumsan dolor nisl sed felis. Duis ultricies risus nec pulvinar dignissim.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec mauris nec nulla congue consectetur nec et odio. Phasellus lobortis commodo mi, in pharetra eros. Nunc eu dictum tellus, vitae finibus dui. Nam aliquet scelerisque quam ac molestie. Suspendisse pellentesque finibus tellus, a malesuada dolor mollis et. Donec eu lectus ac libero iaculis tempus eu vitae magna. </p>
+          </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="outros-produtos">
+    <div class="container">
+      <div class="row d-flex flex-column align-items-center flex-lg-row justify-content-around">
+        <div class="content order-2 order-lg-1">
+          <h3>OUTROS LIVROS</h3>
+          <h2>Perspiciatis Unde Omnis Iste Natus Sit Voluptatem</h2>
+          <p>Perspiciatis unde omnis iste natus sit voluptatem accusantium 
+            doloremque laudantium, totam rem aperiam, eaque ipsa quae. Ab illo 
+            inventore veritatis et quasi architecto beatae vitae dicta sunt 
+            explicabo.Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut 
+            odit aut fugit, sed quia</p>
+          <a href="" class="button1">VER MAIS</a>
+        </div>
+        <div class="image order-1">
+          <img src="./img/banner-02.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="testimonials-section">
+    <div class="container">
+      <div class="row">
+          <h3>RESUMO DO LIVRO</h3>
+          <h2>Donec A Tristique Risus!</h2>
+
+          <div class="content-testimonials d-md-flex flex-wrap justify-content-lg-between">
+
+            <div class="item">
+              <p>“Nunc consequat justo eget enim 
+                  finibus porta. Suspendisse orci nunc, 
+                  rutrum quis nunc sed, feugiat hendrerit 
+                  massa. In cursus ornare sollicitudin.”</p>
+              <span class="autor">Fábio Chiquezi</span>
+              <span class="where">Editora Lorem Ipsun</span>
+            </div>
+            <div class="item">
+
+              <p>“Nunc consequat justo eget enim 
+                  finibus porta. Suspendisse orci nunc, 
+                  rutrum quis nunc sed, feugiat hendrerit 
+                  massa. In cursus ornare sollicitudin.”</p>
+              <span class="autor">Fábio Chiquezi</span>
+              <span class="where">Editora Lorem Ipsun</span>
+            </div>
+
+            <div class="item">
+              <p>“Nunc consequat justo eget enim 
+                  finibus porta. Suspendisse orci nunc, 
+                  rutrum quis nunc sed, feugiat hendrerit 
+                  massa. In cursus ornare sollicitudin.”</p>
+              <span class="autor">Fábio Chiquezi</span>
+              <span class="where">Editora Lorem Ipsun</span>
+            </div>
+              
+            <div class="item">
+              <p>“Nunc consequat justo eget enim 
+                  finibus porta. Suspendisse orci nunc, 
+                  rutrum quis nunc sed, feugiat hendrerit 
+                  massa. In cursus ornare sollicitudin.”</p>
+              <span class="autor">Fábio Chiquezi</span>
+              <span class="where">Editora Lorem Ipsun</span>
+            </div>
+
+              <div class="item">
+                <p>“Nunc consequat justo eget enim 
+                    finibus porta. Suspendisse orci nunc, 
+                    rutrum quis nunc sed, feugiat hendrerit 
+                    massa. In cursus ornare sollicitudin.”</p>
+                <span class="autor">Fábio Chiquezi</span>
+                <span class="where">Editora Lorem Ipsun</span>
+              </div>
+
+                <div class="item">
+                  <p>“Nunc consequat justo eget enim 
+                      finibus porta. Suspendisse orci nunc, 
+                      rutrum quis nunc sed, feugiat hendrerit 
+                      massa. In cursus ornare sollicitudin.”</p>
+                  <span class="autor">Fábio Chiquezi</span>
+                  <span class="where">Editora Lorem Ipsun</span>
+                </div>
+          </div>
+      </div>
+    </div>
+
+    <hr class="hr-1 d-none d-lg-block">
+
+    <footer class="contato-section">
+      <div class="container">
+        <div class="row">
+            <h2>Nunc Conseuat just a finibus?</h2>
+
+            <div class="sub">
+                <p>Nunc consequat justo eget enim finibus porta. Suspendisse orci nunc. Nunc 
+                    consequat justo eget enim finibus porta. Suspendisse orci nunc.</p>
+            </div>
+      
+            <div class="contact-1 d-lg-flex">
+              <div class="tel">
+                <p>(19) 3227-9718</p>
+                <p>(19) 9 9283-7035</p>
+              </div>
+              <ul class="social d-flex">
+                <li><a href="https://www.facebook.com/" target="_blank"><i class="icon icon-face"></i></a></li>
+                <li><a href="https://www.instagram.com/?hl=pt-br" target="_blank"><i class="icon icon-insta"></i></a></li>
+                <li><a href="https://api.whatsapp.com/send?phone=5511983127035" target="_blank"><i class="icon icon-whats"></i></a></li>
+              </div>
+            </div>
+      
+            <p class="ajuste-separa">Ou</p>
+      
+            <form action="">
+              <input type="text" name="nome" placeholder="Nome Completo">
+              <input type="text" name="email" placeholder="E-mail">
+              <textarea name="mensagem" placeholder="Sua Mensagem"></textarea>
+              <input type="text" value="enviar" class="button-submit">
+              <a href="" class="button1">ENVIAR</a>
+            </form>
+      
+            <div class="direitos-autorais">
+              <p>© Todos os direitos autorais reservados.</p>
+            </div>
+        </div>
+      </div>
+    </footer>
+  </section>
+
+
+
+  <?php get_footer(); ?>
